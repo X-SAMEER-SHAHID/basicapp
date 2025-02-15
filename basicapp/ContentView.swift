74a382a5cce242355d32aca85b83012e021d62ff
sameer-shahid <sameershahid5911@gmail.com>
2025-02-15 20:50:58 +0500

    Initial Commit

---
+++ basicapp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  basicapp
+//
+//  Created by Sameer,s Macbook on 2/12/25.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("Hello, world!")
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ basicapp/basicappApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicappApp.swift
+//  basicapp
+//
+//  Created by Sameer,s Macbook on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct basicappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
